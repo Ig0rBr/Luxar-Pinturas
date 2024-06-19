1fb3d6ac38383bbd325322fbda31be5ffb0764dc
Igor <167318979+Ig0rBr@users.noreply.github.com>
2024-06-19 17:47:10 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function bt1(){
+    document.querySelector('img').style.background = "blue"
+}
+function bt2(){
+    document.querySelector('img').style.background = "red"
+}
+function bt4(){
+    document.querySelector('img').style.background = "gray"
+}
+function bt5(){
+    document.querySelector('img').style.background = "white"
+}
+function bt3(){
+    document.querySelector('img').style.background = "green"
+}
+
